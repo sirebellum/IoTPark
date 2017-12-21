202bb44ed9a8973fcc80fa36c2c5f3268553e887
Josh <bluedude227@gmail.com>
2017-12-20 21:59:50 -0600

    Filled out Car class functions
    Former-commit-id: 418806c615f1082389e185cf6a805d94b3b54c06

--- classes.py
+++ classes.py
@@ -11,3 +11,16 @@ class Car:
 		self.delta_pos = 0.1 * self.size
 		self.id = Car.ID
 		Car.ID = Car.ID + 1
+		
+	
+	def distance(self, upper_x, upper_y, lower_x, lower_y): #distance between input and car
+		new_x = (upper_x + lower_x)/2
+		new_y = (upper_y + lower_y)/2
+		return sqrt( pow((new_x - self.x), 2) + pow((new_y - self.y), 2) )
+		
+		
+	def update(self, upper_x, upper_y, lower_x, lower_y):
+		self.x = (upper_x + lower_x)/2
+		self.y = (upper_y + lower_y)/2
+		self.size = math.sqrt( pow((upper_x - lower_x), 2) + pow((upper_y - lower_y), 2) )
+		self.delta_pos = 0.1 * self.size
