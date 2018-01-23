d80a4762e357270da11106e2e04b45096eeb20bb
Josh <bluedude227@gmail.com>
2018-01-23 16:01:44 -0600

    Convert model for use on Jetson
    Former-commit-id: 006bca0a7c243193267dbeac03a35df2de380cbf

---
+++ training/frozen/convert_to_uff.py
@@ -0,0 +1,17 @@
+# Import TensorRT Modules
+import tensorrt as trt
+import uff
+import os
+from tensorrt.parsers import uffparser
+G_LOGGER = trt.infer.ConsoleLogger(trt.infer.LogSeverity.INFO)
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("frozen_model", help="Specify model ie. rcnn_resnet_ww48c")
+args = parser.parse_args()
+
+CWD_PATH = os.getcwd()
+PATH_TO_CKPT = os.path.join(CWD_PATH, args.frozen_model, 'frozen_inference_graph.pb')
+
+# Load your newly created Tensorflow frozen model and convert it to UFF
+uff_model = uff.from_tensorflow_frozen_model(PATH_TO_CKPT, ["detection_scores:0"])
