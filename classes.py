b8725edf6e8f24f5969e3a2c085ab7bc48cb3f07
Josh <bluedude227@gmail.com>
2018-01-24 09:22:49 -0600

    Implemented delete self function
    Former-commit-id: bd39e44c605b8e4206baffbbadaf1b036d6208ed

--- classes.py
+++ classes.py
@@ -14,26 +14,31 @@ class Car:
         self.id = Car.ID
         Car.ID = Car.ID + 1
         self.detected = 1
-        
-    
+
+
     def distance(self, upper_x, upper_y, lower_x, lower_y): #distance between input and car
         new_x = (upper_x + lower_x)/2
         new_y = (upper_y + lower_y)/2
         return math.sqrt( pow((new_x - self.x), 2) + pow((new_y - self.y), 2) )
-        
-        
+
+
     def update(self, upper_x, upper_y, lower_x, lower_y, FPS):
         self.x = (upper_x + lower_x)/2
         self.y = (upper_y + lower_y)/2
         self.size = math.sqrt( pow((upper_x - lower_x), 2) + pow((upper_y - lower_y), 2) )
         self.delta_pos = 0.15 * self.size
-        if self.detections < 30:        
+        if self.detections < 30:
             self.detections = self.detections + 1
         self.persistence = 0.5*FPS*math.exp(self.detections*0.1)
         self.detected = 1
+
 
     def update_persistence(self, res_x, res_y):
         if (self.x < res_x*0.1 or self.x > res_x*0.9) \
         or (self.y < res_y*0.1 or self.y > res_y*0.9): #If close to edge, substract double
             self.persistence = self.persistence - 1
         self.persistence = self.persistence - 1
+
+
+    def kill(self):
+        del self
